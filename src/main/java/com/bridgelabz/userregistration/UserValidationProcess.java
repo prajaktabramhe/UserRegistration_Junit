22e013b4ee4cb648df48ca5f09231d10d1171b06
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-25 01:44:39 +0530

    [Prajakta]: Refactored the code to used Lambda function to validated user entry such as firstName,LastName,Mobile,Password,Email

--- src/main/java/com/bridgelabz/userregistration/UserValidationProcess.java
+++ src/main/java/com/bridgelabz/userregistration/UserValidationProcess.java
@@ -1,120 +1,70 @@
 package com.bridgelabz.userregistration;
 
+@FunctionalInterface
+interface lambdaValidationFun
+{
+boolean matchPatternLambda(String input, String pattern);
+}
+
 public class UserValidationProcess
 {
+    lambdaValidationFun dd = (pattern, input) ->  pattern.matches(input);
 
-    //for firstName validation
 
+//for firstName validation
     public boolean validateFirstName(String name)
     {
-        String pattern = "^([A-Z]{1})([a-z]{2,})$";
-
-        if (name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String firstnamepattern = "^([A-Z]{1})([a-z]{2,})$";
+        return dd.matchPatternLambda(name,firstnamepattern);
     }
 
     //for lastName validation
 
     public boolean validateLastName(String name)
     {
-        String pattern = "^([A-Z]{1})([a-z]{2,})$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String lastnamepattern = "^([A-Z]{1})([a-z]{2,})$";
+        return dd.matchPatternLambda(name,lastnamepattern);
     }
 
 
     //for Email validation
     public boolean validateEmail(String name)
     {
-        String pattern =  "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String emailpattern =  "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
+        return dd.matchPatternLambda(name,emailpattern);
     }
 
     //for Mobile No. validation
     public boolean validateMobileNumber(String name)
     {
-        String pattern =  "^([0-9]{2})[ ]([1-9]{1}[0-9]{9})$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String mobilepattern =  "^([0-9]{2})[ ]([1-9]{1}[0-9]{9})$";
+        return dd.matchPatternLambda(name,mobilepattern);
     }
 
     //for Password validation
     public boolean validatePassword(String name)
     {
-        String pattern = "^[a-zA-Z0-9]{8,}$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String pwdpattern = "^[a-zA-Z0-9]{8,}$";
+        return dd.matchPatternLambda(name,pwdpattern);
     }
 
 
     public boolean passwordAtLeastOneUpperCase(java.lang.String name)
     {
-        String pattern =  "^(.*[A-Z]{1}[A-Za-z0-9]{7,})$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String pwdatleastoneuppercasepattern =  "^(.*[A-Z]{1}[A-Za-z0-9]{7,})$";
+        return dd.matchPatternLambda(name,pwdatleastoneuppercasepattern);
     }
 
     public boolean validateAlteastOneNumber(java.lang.String name)
     {
-        String pattern = "^(.*[A-Z]{1,}[0-9]{1,}[A-Za-z0-9]{6,})$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String atleastonenumberpattern = "^(.*[A-Z]{1,}[0-9]{1,}[A-Za-z0-9]{6,})$";
+        return dd.matchPatternLambda(name,atleastonenumberpattern);
     }
 
     public boolean validateSpecialCharacter(java.lang.String name)
     {
-        String pattern = "^(.*[A-Z]{1,}.*[0-9]{1,}.*[~!@#$%^&*]?[A-Za-z0-9]{5,})$";
-        if( name.matches(pattern))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        String specialcharacterpattern = "^(.*[A-Z]{1,}.*[0-9]{1,}.*[~!@#$%^&*]?[A-Za-z0-9]{5,})$";
+        return dd.matchPatternLambda(name,specialcharacterpattern);
     }
 
     public String analyseMood(String message) {
